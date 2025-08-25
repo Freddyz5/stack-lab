f98862fac50f763e2f75f687f33858d7cba775aa
RUBENTOMALA <ruben.al972@hotmail.com>
2025-08-25 17:30:28 -0500

    feat(db): :sparkles: Add games table to the database schema

--- packages/client-db/drizzle/0000_add_games_and_users_table.sql
+++ packages/client-db/drizzle/0000_add_games_and_users_table.sql
@@ -1,3 +1,14 @@
+CREATE TABLE "games" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"description" text NOT NULL,
+	"genre" text DEFAULT '',
+	"difficulty" text DEFAULT '',
+	"state" text DEFAULT '',
+	"isActive" boolean NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"modifiedAt" timestamp NOT NULL
+);
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY NOT NULL,
