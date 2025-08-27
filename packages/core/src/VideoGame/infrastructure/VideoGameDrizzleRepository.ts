a8215783ab23daa61140a8761090a9681e312de1
RUBENTOMALA <ruben.al972@hotmail.com>
2025-08-27 17:12:59 -0500

    feat(core): :sparkles: Implement VideoGameDrizzleRepository for video game management

---
+++ packages/core/src/VideoGame/infrastructure/VideoGameDrizzleRepository.ts
@@ -0,0 +1,93 @@
+import { db, games as videoGames, eq } from 'client-db';
+import { VideoGame } from '../domain/VideoGame';
+import { VideoGameId } from '../domain/VideoGameId';
+import { Nullable } from 'src/shared/domain/value-object/Nullable';
+import { VideoGameTitle } from '../domain/VideoGameTitle';
+
+export class VideoGameDrizzleRepository implements VideoGameDrizzleRepository {
+  protected table: typeof videoGames = videoGames;
+
+  async save(videoGame: VideoGame): Promise<void> {
+    const currentVideoGame = await db
+      .select()
+      .from(videoGames)
+      .where(eq(videoGames.id, videoGame.id.value));
+
+    if (currentVideoGame.length !== 0) {
+      await db
+        .update(videoGames)
+        .set({
+          ...videoGame.toPrimitives(),
+          modifiedAt: new Date(),
+        })
+        .where(eq(videoGames.id, videoGame.id.value));
+
+      console.log('Video game updated! 🎮');
+    } else {
+      await db.insert(videoGames).values({
+        ...videoGame.toPrimitives(),
+        modifiedAt: new Date(),
+      });
+
+      console.log('New video game created! 🕹️');
+    }
+
+    return Promise.resolve();
+  }
+
+  async findById(id: VideoGameId): Promise<Nullable<VideoGame>> {
+    const currentvideoGame = await db.select().from(videoGames).where(eq(videoGames.id, id.value));
+
+    return VideoGame.fromPrimitives({
+      ...currentvideoGame[0],
+      genre: currentvideoGame[0].genre ?? '',
+      difficulty: currentvideoGame[0].difficulty ?? '',
+      state: currentvideoGame[0].state ?? '',
+    });
+  }
+
+  async findByTitle(title: VideoGameTitle): Promise<Nullable<VideoGame>> {
+    const result = await db.query.games.findFirst({
+      where: eq(videoGames.title, title.value)
+    });
+    if (!result) {
+      return null;
+    }
+
+    return VideoGame.fromPrimitives({
+      ...result,
+      genre: result.genre ?? '',
+      difficulty: result.difficulty ?? '',
+      state: result.state ?? '',
+    })
+  }
+
+  async findAll(): Promise<VideoGame[]> {
+    const currentVideoGames = await db
+    .select()
+    .from(videoGames)
+    .where(eq(videoGames.isActive, true));
+
+    return currentVideoGames.map((videoGame) =>
+      VideoGame.fromPrimitives({
+        ...videoGame,
+        genre: videoGame.genre ?? '',
+        difficulty: videoGame.difficulty ?? '',
+        state: videoGame.state ?? '',
+      })
+    );
+  }
+
+  async delete(id: VideoGameId): Promise<void> {
+    await db
+      .update(videoGames)
+      .set({
+        isActive: false,
+        modifiedAt: new Date(),
+      })
+      .where(eq(videoGames.id, id.value));
+
+    return Promise.resolve();
+  }
+
+}
---
+++ packages/core/src/VideoGame/infrastructure/index.ts
@@ -0,0 +1,1 @@
+export {VideoGameDrizzleRepository} from './VideoGameDrizzleRepository';
