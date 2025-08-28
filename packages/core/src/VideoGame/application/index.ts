b217dd226d902b3fb50c3a7af62c8c6352077fdb
RUBENTOMALA <ruben.al972@hotmail.com>
2025-08-28 10:29:58 -0500

    feat(core): :sparkles: Add VideoGame module exports for application and infrastructure

---
+++ packages/core/src/VideoGame/application/index.ts
@@ -0,0 +1,6 @@
+export {VideoGameCreate} from './Create/VideoGameCreate';
+export {VideoGameDelete} from './Delete/VideoGameDelete';
+export {VideoGameFindById} from './Find/VideoGameFindById';
+export {VideoGamesSearch} from './Search/VideoGamesSearch';
+export {VideoGameUpdate} from './Update/VideoGameUpdate';
+export {VideoGameFindByTitle} from './Find/VideoGameFindByTitle';
