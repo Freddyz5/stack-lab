82fe7da93afb4a37db7c85e9e7b2c589206d9b5b
Freddyz5 <freddyltacuri@gmail.com>
2025-09-04 11:36:32 -0500

    fix(web): :bug: Add invalidateQueries to reload videogames

--- apps/web/src/Games/hooks/useAddVideoGame.ts
+++ apps/web/src/Games/hooks/useAddVideoGame.ts
@@ -1,14 +1,16 @@
 import {createVideoGame} from "../services/createVideoGame";
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {useSnack} from "../../shared/hooks/useSnack";
 
 export const useAddVideoGame = () => {
     const {enqueueSnack} = useSnack();
+    const queryClient = useQueryClient();
 
     const {mutate: addVideoGame, isSuccess, isPending, error}=useMutation({
         mutationFn: createVideoGame,
         onSuccess: () => {
             enqueueSnack(`Videojuego creado exitosamente!`, "success");
+            queryClient.invalidateQueries({ queryKey: ['videoGames'] });
         },
         onError: (error: Error) => {
             enqueueSnack(error.message, "error");
